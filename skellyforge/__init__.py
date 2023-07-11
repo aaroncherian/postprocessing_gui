911842d903ac19074f856595620d034d6615d0a3
Jon Matthis <jonmatthis@gmail.com>
2023-07-11 18:54:25 -0500

    update __init__ installs so pyinstaller might work

--- skellyforge/__init__.py
+++ skellyforge/__init__.py
@@ -11,16 +11,23 @@ __repo_url__ = (
 )
 __repo_issues_url__ = f"{__repo_url__}issues"
 
-import sys
-from pathlib import Path
+from skellyforge.freemocap_utils.config import default_settings
+from skellyforge.freemocap_utils.constants import TASK_FILTERING, PARAM_CUTOFF_FREQUENCY, PARAM_SAMPLING_RATE, \
+    PARAM_ORDER, PARAM_ROTATE_DATA, TASK_SKELETON_ROTATION, TASK_INTERPOLATION, TASK_FINDING_GOOD_FRAME
+
+from skellyforge.freemocap_utils.postprocessing_widgets.task_worker_thread import TaskWorkerThread
+
 
 print(f"Thank you for using {__package_name__}!")
 print(f"This is printing from: {__file__}")
 print(f"Source code for this package is available at: {__repo_url__}")
 
-base_package_path = Path(__file__).parent
-print(f"adding base_package_path: {base_package_path} : to sys.path")
-sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
+# import sys
+# from pathlib import Path
+
+# base_package_path = Path(__file__).parent
+# print(f"adding base_package_path: {base_package_path} : to sys.path")
+# sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
 
 
 
